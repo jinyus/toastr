977eb64a641591d2c187d3a70561b9b8b715accb
jinyus <starboymc4@gmail.com>
2023-01-04 19:32:41 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import toast, { Toaster } from 'svelte-french-toast';
+	const opts = { id: 'my-toast' };
+	const showToast = () => {
+		toast.loading('please wait...', opts);
+		setTimeout(() => {
+			toast.success('Success', opts);
+		}, 2000);
+	};
+</script>
+
+<h1>Toast Demo</h1>
+<button on:click={showToast}> Show toast </button>
+
+<Toaster />
