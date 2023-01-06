94be7e9629359f985d5ce409643451927395ae59
jinyus <starboymc4@gmail.com>
2023-01-06 13:26:51 -0500

    add build to github

---
+++ build/_app/immutable/chunks/1-b6377816.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-7a4d3bba.js";export{t as component};
---
+++ build/_app/immutable/chunks/2-8a2b7553.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-e2f0c57b.js";export{t as component};
